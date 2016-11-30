9ac58dc29aa5c8fed4e2741399d9e63dc3bc784a
kazuhisaokada <kazzz1215@gmail.com>
2016-11-30 16:21:08 +0000

    add profile and area

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :check_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,10 +20,29 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'プロフィールを更新しました'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :profile, :password, :password_confirmation)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def check_user
+    redirect_to root_path if @user != current_user
+  end
 end
